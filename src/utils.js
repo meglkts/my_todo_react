257e88bb9fc01aa9236e28685d5a1c432b5eb0c3
Meg Kline-Tuls <meglktuls@gmail.com>
2017-02-17 23:14:08 -0800

    adds isArray to utils and incoroprate into clone

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,18 @@
+const isArray = (thing) => {
+  return Object.prototype.toString.call(thing) === '[object Array]'
+}
+
 const clone = (obj) => {
   const keys = Object.keys(obj)
+  let acc = {}
+  if (isArray(obj)) acc = []
   return keys.reduce((p, c) => {
     p[c] = obj[c]
     return p
-  }, {})
+  }, acc)
 }
 
 module.exports = {
-  clone
+  clone,
+  isArray
 }
--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,14 +1,29 @@
 const { equal, deepEqual, notEqual } = require('assert')
-const { clone } = require('./utils')
+const { clone, isArray } = require('./utils')
 
 describe('utils.js', () => {
   describe('clone()', () => {
-    fit('should return an unreferenced copy of an object', () => {
+    it('should return an unreferenced copy of an object', () => {
       const obj = { foo: 'bar' }
       const copy = clone(obj)
       equal(obj, obj)
       notEqual(obj, copy)
       deepEqual(obj, copy)
+    })
+
+    it('should return an unreferenced copy of an array', () => {
+      const a = [{ foo: 'bar' }]
+      const copy = clone(a)
+      equal(a, a)
+      notEqual(a, copy)
+      deepEqual(a, copy)
+      equal(isArray(copy), true)
+    })
+  })
+  describe('isArray()', () => {
+    it('should return true if input is array [], false if object {}', () => {
+      equal(isArray(['foo', 'bar']), true)
+      equal(isArray({foo: 'bar'}), false)
     })
   })
 })
