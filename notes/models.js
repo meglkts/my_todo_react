77a36df8e528242c5504278379b382170bf0d4d3
Meg Kline-Tuls <meglktuls@gmail.com>
2017-02-17 22:15:59 -0800

    adds notes on models and views

---
+++ notes/models.js
@@ -0,0 +1,19 @@
+const app = {
+  title: 'todos',
+  newTaskPlaceholder: 'What needs to be done?',
+  taskList
+}
+
+const taskList = {
+  tasks: [task]
+  lastNoteAdded: 1487364276558
+}
+
+const task = {
+  id: 'adfs-134-adsf-123',
+  text: 'laundry',
+  status: 'active',
+  active: true,
+  completed: false,
+  created: 1487364276558
+}
