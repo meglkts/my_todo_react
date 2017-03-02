a344f1ac5b83988cc641421682fdc4cb6ddb1725
Meg Kline-Tuls <meglktuls@gmail.com>
2017-03-02 09:23:51 -0800

    tweaks styles, adds action notes

---
+++ notes/actions.js
@@ -0,0 +1,9 @@
+/*
+toggle select all / deselect All
+write task (on enter), {task}
+select / deselect task (on click), {id}
+edit task (double click) --> write task
+delete task (on click), {id}
+filter (on click), {filter}
+clear completed (on click)
+*/
